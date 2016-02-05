9d4b7691338928cf967a9b8ad672e4aefdc2f243
Abdelkrime Aries <kariminfo0@gmail.com>
2016-02-05 21:04:30 +0100

    add Arabic translation

--- Assajjaa/Media/i18n/ar.ts
+++ Assajjaa/Media/i18n/ar.ts
@@ -2,40 +2,312 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ar">
 <context>
+    <name>About</name>
+    <message>
+        <location filename="../../Ui/about.ui" line="44"/>
+        <source>About ...</source>
+        <translation>عن البرنامج...</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/about.ui" line="92"/>
+        <source>License</source>
+        <translation>ترخيص</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/about.ui" line="129"/>
+        <source>Authors</source>
+        <translation>المؤلفين</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/about.ui" line="163"/>
+        <source>Thanks</source>
+        <translation>شكر</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/about.cpp" line="76"/>
+        <source>Version</source>
+        <translation>النسخة</translation>
+    </message>
+</context>
+<context>
+    <name>ExportResult</name>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="59"/>
+        <source>Pdf file</source>
+        <translation>ملف بي دي آف</translation>
+    </message>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="61"/>
+        <source>OpenDocument file</source>
+        <translation>ملف المستندات المفتوحة</translation>
+    </message>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="63"/>
+        <source>HTML file</source>
+        <translation>ملف ويب</translation>
+    </message>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="75"/>
+        <source>File already exists</source>
+        <translation>ملف موجود مسبقا</translation>
+    </message>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="77"/>
+        <source>This file exists, do you want to overwrite it?</source>
+        <translation>هذا الملف موجود، هل تريد استبداله؟</translation>
+    </message>
+</context>
+<context>
+    <name>HelpViewer</name>
+    <message>
+        <location filename="../../Ui/helpviewer.cpp" line="203"/>
+        <source>Open Link in New Tab</source>
+        <translation>افتح الرابط في نافذة جديدة</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../mainwindow.ui" line="14"/>
-        <source>MainWindow</source>
+        <location filename="../../Ui/mainwindow.ui" line="26"/>
+        <source>Assajjaa: Arabic rhyme search</source>
+        <translation>السجّاع: البحث عن القوافي العربية</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="74"/>
+        <source>&amp;File</source>
+        <translation>ملف</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="84"/>
+        <source>&amp;Help</source>
+        <translation>مساعدة</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="95"/>
+        <source>&amp;View</source>
+        <translation>عرض</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="99"/>
+        <source>Zoom</source>
+        <translation>حجم</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="114"/>
+        <source>&amp;Edit</source>
+        <translation>تحرير</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="137"/>
+        <source>Main Toolbar</source>
+        <translation>الشريط الرئيسي</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="175"/>
+        <source>Zoom Toolbar</source>
+        <translation>شريط أدوات الحجم</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="203"/>
+        <source>&amp;Close</source>
+        <translation>إغلاق</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="213"/>
+        <source>&amp;About</source>
+        <translation>عن البرنامج</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="223"/>
+        <source>&amp;Preference</source>
+        <translation>خيارات</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="233"/>
+        <source>Help Content</source>
+        <translation>محتوى المساعدة</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="245"/>
+        <source>Content Export</source>
+        <translation>تصدير المحتوى</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="258"/>
+        <source>&amp;Print</source>
+        <translation>طباعة</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="268"/>
+        <source>Zoom In</source>
+        <translation>تكبير</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="278"/>
+        <source>Zoom Out</source>
+        <translation>تصغير</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="288"/>
+        <source>Normal Size</source>
+        <translation>حجم عادي</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="298"/>
+        <source>Hide</source>
+        <translation>إخفاء</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="307"/>
+        <source>Contact us</source>
+        <translation>إتصل بنا</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="316"/>
+        <source>Homepage</source>
+        <translation>الصفحة الرئيسية</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.cpp" line="94"/>
+        <location filename="../../Ui/mainwindow.cpp" line="292"/>
+        <source>Show</source>
+        <translation>إظهار</translation>
+    </message>
+</context>
+<context>
+    <name>Preference</name>
+    <message>
+        <location filename="../../Ui/preference.ui" line="32"/>
+        <source>Preferences</source>
+        <translation>خيارات</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="67"/>
+        <source>User Interface</source>
+        <translation>واجهة المستخدم</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="82"/>
+        <source>User Interface language</source>
+        <translation>لغة واجهة المستخدم</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="111"/>
+        <source>Tables style</source>
+        <translation>ستايل الجداول</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="130"/>
+        <source>Content Export</source>
+        <translation>تصدير المحتوى</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="142"/>
+        <source>Exported tables</source>
+        <translation>الجداول المصدرة</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="203"/>
+        <source>Use Style</source>
+        <translation>استعمال الستايل</translation>
+    </message>
+</context>
+<context>
+    <name>SrchFrame</name>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="14"/>
+        <source>Frame</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="27"/>
+        <location filename="../../Ui/srchframe.ui" line="58"/>
+        <source>Input the rhyme</source>
+        <translation>إدخال القافية</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="63"/>
+        <source>Choose the rhyme</source>
+        <translation>اختيار القافية</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="153"/>
+        <source>No templates</source>
+        <translation>بلا أوزان</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="158"/>
+        <source>Choose the template</source>
+        <translation>اختيار الوزن</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="216"/>
+        <source>No begining</source>
+        <translation>بلا بداية</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="221"/>
+        <source>Input the begining</source>
+        <translation>اختيار البداية</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="277"/>
         <source>Search</source>
+        <translation>بحث</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="308"/>
+        <source>Found words</source>
+        <translation>الكلمات الناتجة</translation>
+    </message>
+</context>
+<context>
+    <name>jpconjhelp</name>
+    <message>
+        <location filename="../../Ui/help.ui" line="14"/>
+        <source>JapKatsuyou Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="41"/>
-        <source>None</source>
+        <location filename="../../Ui/help.ui" line="52"/>
+        <source>File</source>
+        <translation>ملف</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/help.ui" line="73"/>
+        <source>Contents</source>
+        <translation>المحتويات</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/help.ui" line="90"/>
+        <source>Index</source>
+        <translation>فهرس</translation>
+    </message>
+    <message>
+        <location filename="../../Ui/help.ui" line="100"/>
+        <source>Look for</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <location filename="../../mainwindow.ui" line="46"/>
-        <source>ال</source>
+    <message>
+        <location filename="../../Ui/help.ui" line="112"/>
+        <source>Close</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <location filename="../../mainwindow.ui" line="51"/>
-        <source>ام</source>
+</context>
+<context>
+    <name>jpconjmain</name>
+    <message>
+        <location filename="../../Ui/preference.cpp" line="87"/>
+        <source>Standard Conjugation</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <location filename="../../mainwindow.ui" line="56"/>
-        <source>يل</source>
+    <message>
+        <location filename="../../Ui/preference.cpp" line="89"/>
+        <source>Basic Conjugation</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <location filename="../../mainwindow.ui" line="71"/>
-        <source>متفاعل</source>
+    <message>
+        <location filename="../../Ui/preference.cpp" line="91"/>
+        <source>Complex Conjugation</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- Assajjaa/Media/i18n/fr.ts
+++ Assajjaa/Media/i18n/fr.ts
@@ -2,40 +2,312 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="fr">
 <context>
-    <name>MainWindow</name>
+    <name>About</name>
     <message>
-        <location filename="../../mainwindow.ui" line="14"/>
-        <source>MainWindow</source>
+        <location filename="../../Ui/about.ui" line="44"/>
+        <source>About ...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="27"/>
+        <location filename="../../Ui/about.ui" line="92"/>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/about.ui" line="129"/>
+        <source>Authors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/about.ui" line="163"/>
+        <source>Thanks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/about.cpp" line="76"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ExportResult</name>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="59"/>
+        <source>Pdf file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="61"/>
+        <source>OpenDocument file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="63"/>
+        <source>HTML file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="75"/>
+        <source>File already exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../UiHelper/msg.h" line="77"/>
+        <source>This file exists, do you want to overwrite it?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HelpViewer</name>
+    <message>
+        <location filename="../../Ui/helpviewer.cpp" line="203"/>
+        <source>Open Link in New Tab</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="26"/>
+        <source>Assajjaa: Arabic rhyme search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="74"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="84"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="95"/>
+        <source>&amp;View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="99"/>
+        <source>Zoom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="114"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="137"/>
+        <source>Main Toolbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="175"/>
+        <source>Zoom Toolbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="203"/>
+        <source>&amp;Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="213"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="223"/>
+        <source>&amp;Preference</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="233"/>
+        <source>Help Content</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="245"/>
+        <source>Content Export</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="258"/>
+        <source>&amp;Print</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="268"/>
+        <source>Zoom In</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="278"/>
+        <source>Zoom Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="288"/>
+        <source>Normal Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="298"/>
+        <source>Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="307"/>
+        <source>Contact us</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.ui" line="316"/>
+        <source>Homepage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/mainwindow.cpp" line="94"/>
+        <location filename="../../Ui/mainwindow.cpp" line="292"/>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Preference</name>
+    <message>
+        <location filename="../../Ui/preference.ui" line="32"/>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="67"/>
+        <source>User Interface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="82"/>
+        <source>User Interface language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="111"/>
+        <source>Tables style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="130"/>
+        <source>Content Export</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="142"/>
+        <source>Exported tables</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.ui" line="203"/>
+        <source>Use Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SrchFrame</name>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="14"/>
+        <source>Frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="58"/>
+        <source>Input the rhyme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="63"/>
+        <source>Choose the rhyme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="153"/>
+        <source>No templates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="158"/>
+        <source>Choose the template</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="216"/>
+        <source>No begining</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="221"/>
+        <source>Input the begining</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/srchframe.ui" line="277"/>
         <source>Search</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../mainwindow.ui" line="41"/>
-        <source>None</source>
+        <location filename="../../Ui/srchframe.ui" line="308"/>
+        <source>Found words</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <location filename="../../mainwindow.ui" line="46"/>
-        <source>ال</source>
+</context>
+<context>
+    <name>jpconjhelp</name>
+    <message>
+        <location filename="../../Ui/help.ui" line="14"/>
+        <source>JapKatsuyou Help</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <location filename="../../mainwindow.ui" line="51"/>
-        <source>ام</source>
+    <message>
+        <location filename="../../Ui/help.ui" line="52"/>
+        <source>File</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <location filename="../../mainwindow.ui" line="56"/>
-        <source>يل</source>
+    <message>
+        <location filename="../../Ui/help.ui" line="73"/>
+        <source>Contents</source>
         <translation type="unfinished"></translation>
     </message>
-    <message utf8="true">
-        <location filename="../../mainwindow.ui" line="71"/>
-        <source>متفاعل</source>
+    <message>
+        <location filename="../../Ui/help.ui" line="90"/>
+        <source>Index</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/help.ui" line="100"/>
+        <source>Look for</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/help.ui" line="112"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>jpconjmain</name>
+    <message>
+        <location filename="../../Ui/preference.cpp" line="87"/>
+        <source>Standard Conjugation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.cpp" line="89"/>
+        <source>Basic Conjugation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Ui/preference.cpp" line="91"/>
+        <source>Complex Conjugation</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
